901b5c11a5ab403cf27cd8ed9af1457d4c4e33ce
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-07-23 01:30:35 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+document.onmousemove = function(event){
+    var x = (window.innerWidth / 2 - event.x);
+    var y = (window.innerHeight / 2 - event.y);
+    console.log(x + " " + y);
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    document.querySelector('.gradient-button').style.transform = 'rotate3d('+y+', '+x+',  0, 50deg)';
+   
+}
 
+  
+ /* document.onmousemove = function(event) {
+    var x = (window.innerWidth / 2 - event.x) / 10;
+    var y = (window.innerHeight / 2 - event.y) / 10;
+    console.log(x + " " + y);
+    document.querySelector('.gradient-button').style.transform = 'rotateX('+y+' deg) rotateY('+x+'deg)';
+};*/
