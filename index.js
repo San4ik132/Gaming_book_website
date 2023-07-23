3caccf2ea100bab9c153db8df83b5d71a30b1a22
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-07-23 18:38:54 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-document.onmousemove = function(event){
-    var x = (window.innerWidth / 2 - event.x);
-    var y = (window.innerHeight / 2 - event.y);
-    console.log(x + " " + y);
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
-    document.querySelector('.gradient-button').style.transform = 'rotate3d('+y+', '+x+',  0, 50deg)';
-   
-}
+const moreBtn = document.getElementById("more-btn");
+const h1 = document.getElementById("h1");
 
-  
- /* document.onmousemove = function(event) {
-    var x = (window.innerWidth / 2 - event.x) / 10;
-    var y = (window.innerHeight / 2 - event.y) / 10;
-    console.log(x + " " + y);
-    document.querySelector('.gradient-button').style.transform = 'rotateX('+y+' deg) rotateY('+x+'deg)';
-};*/
+const rotateBtn = (document.onmousemove = (e) => {
+  var y = Math.round(window.innerHeight / 2 - e.y);
+  var x = Math.round(window.innerWidth / 2 - e.x);
+  moreBtn.style.transform = `rotate3d(${-y}, ${x}, 0, 12deg)`;
+  moreBtn.style.transition = "none";
+});
+
+const noneRotateBtn = document.addEventListener("mouseout", (e) => {
+  if (e.relatedTarget === null) {
+    moreBtn.style.transform = "rotate3d(0, 0, 0, 0deg)";
+    moreBtn.style.transition = "0.3s";
+  }
+});
+
+// Проверяем, было ли выполнено обновление страницы
+if (performance.navigation.type === 1) {
+    // Обновление страницы произошло, перенаправляем на начальную страницу
+    window.location.href = "index.html";
+  }
