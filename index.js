86e4c06d3e543821fafbc2173969257f272a409b
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-07-23 19:12:36 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const h1 = document.getElementById("h1");
 const rotateBtn = (document.onmousemove = (e) => {
   var y = Math.round(window.innerHeight / 2 - e.y);
   var x = Math.round(window.innerWidth / 2 - e.x);
-  moreBtn.style.transform = `rotate3d(${-y}, ${x}, 0, 12deg)`;
-  moreBtn.style.transition = "none";
+  moreBtn.style.transform = `rotate3d(${-y}, ${x}, -10, 25deg)`;
+  moreBtn.style.transition = ".5s";
 });
 
 const noneRotateBtn = document.addEventListener("mouseout", (e) => {
   if (e.relatedTarget === null) {
     moreBtn.style.transform = "rotate3d(0, 0, 0, 0deg)";
-    moreBtn.style.transition = "0.3s";
+    moreBtn.style.transition = ".5s";
+  }
+});
+const ClickRotateBtn = (document.onmouseout = (e) => {
+  if(moreBtn.offsetHeight >= e.x && moreBtn.offsetWidth <= e.x){
+    moreBtn.style.transform = "rotate3d(0, 0, 0, 0deg)";
+    moreBtn.style.transition = ".5s";
   }
 });
 
