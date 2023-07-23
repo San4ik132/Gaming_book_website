46cd64c25b004b47e6f5be32f15d5abf74fcbec1
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-07-23 23:38:44 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const h1 = document.getElementById("h1");
 const rotateBtn = (document.onmousemove = (e) => {
   var y = Math.round(window.innerHeight / 2 - e.y);
   var x = Math.round(window.innerWidth / 2 - e.x);
-  moreBtn.style.transform = `rotate3d(${-y}, ${x}, -10, 25deg)`;
+  moreBtn.style.transform = `rotate3d(${-y}, ${x}, 0, 25deg)`;
   moreBtn.style.transition = ".5s";
 });
 
@@ -15,7 +15,7 @@ const noneRotateBtn = document.addEventListener("mouseout", (e) => {
   }
 });
 const ClickRotateBtn = (document.onmouseout = (e) => {
-  if(moreBtn.offsetHeight >= e.x && moreBtn.offsetWidth <= e.x){
+  if (moreBtn.offsetHeight >= e.x && moreBtn.offsetWidth <= e.x) {
     moreBtn.style.transform = "rotate3d(0, 0, 0, 0deg)";
     moreBtn.style.transition = ".5s";
   }
@@ -23,6 +23,6 @@ const ClickRotateBtn = (document.onmouseout = (e) => {
 
 // Проверяем, было ли выполнено обновление страницы
 if (performance.navigation.type === 1) {
-    // Обновление страницы произошло, перенаправляем на начальную страницу
-    window.location.href = "index.html";
-  }
+  // Обновление страницы произошло, перенаправляем на начальную страницу
+  window.location.href = "index.html";
+}
